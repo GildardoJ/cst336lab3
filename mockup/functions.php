bb09ab9e471f9c08f3c97e5c8a750ac5cf84693f
Gildardo Orozco <gorozco@csumb.edu>
2017-09-22 20:48:45 +0000

    point winner

--- mockup/functions.php
+++ mockup/functions.php
@@ -1,8 +1,5 @@
 <?php
 
-
-
-
 $deck = range(0,51);  //creates array with values 1 to 52
 
 $suits = array("clubs","spades","hearts","diamonds");
@@ -13,38 +10,46 @@ shuffle($deck);
     $playerTotal = 0;  // total points for each player
     $totalPoints = 0;
     $i= 0; // what player we are on.
-    $players = array(0,0,0,0);  // 
+    $players = array(0,0,0,0);  // 4 players
     
-    echo " < div>";
+    //echo " < div>";
 
-foreach ($deck as $card) {
-    
-   // for(){
-    //
-       // echo "Card value: "  . (($card % 13) + 1) . "-  Card Suite: " .  $suits[floor($card / 13)] . " <br />";
+foreach ($deck as $card) { // for loop
         
         echo "<img class='image-object' src='../img/".$suits[floor($card / 13)]."/".(($card % 13) + 1).".png' alt='Picture of card'>";
         
-        $playerTotal += (($card % 13) + 1); // 
-        $totalPoints += (($card % 13) + 1); // Game total points
+        if($playerTotal > 35){
+            $players[$i] = $playerTotal;
+            $i ++;
+            
+            echo $playerTotal;
+            echo " <br />";
+            //echo " </div>";
+            //echo " <div> ";
+            $playerTotal = 0;
+        }
         
-//    }
-    
-    if($maxCount > 37 ){
+        if($i >3){
+            break;
+        }
+        $playerTotal += (($card % 13) + 1); // <!-- keep track of each players --> 
         
-        $players[i] = $playerTotal;
-        $playerTotal = 0;
-        $i += 1;
         
-        echo " <br />";
-        echo " </div>";
-        echo " <div> ";
+        $totalPoints += (($card % 13) + 1); //  <!-- Game total points for all players-->
         
-    }
-    
-    
 }
 
-    echo " </div> ";
+    $winningIndex = 0;
+    $biggerNum = $players[0];
+    for($i = 0; $i < 4; $i++){
+        if($players[$i] > $biggerNum){
+            $biggerNum = $players[$i];
+            $winningIndex = $i;
+        }
+}
+
+    echo "Player " . ($winningIndex + 1) . " wins";
+
+    //echo " </div> ";
 
 ?>
